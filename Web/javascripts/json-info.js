bf5c38cee2cb0df1ed69ef46a2be90e55dac52a5
Daniel Cavalcanti <dcc69eyes@gmail.com>
2012-10-16 22:59:51 -0300

    Primeiro Commit

---
+++ Web/javascripts/json-info.js
@@ -0,0 +1,4 @@
+﻿var art = '{ "artigos" : [' +
+'{ "id":"info001", "titulo":"Erliquiose: a doença do carrapato" , "texto":"Blablabla" },' +
+'{ "id":"info002", "titulo":"Oftalmologia" , "texto":"Blablabla" },' +
+'{ "id":"info003", "titulo":"Anestesia inalatória" , "texto":"Blablabla" }]}';
